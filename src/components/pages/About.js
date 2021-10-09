82a065849849b35eaa779cf576fea7981119c7fa
ali.sh <narenje20142014@gmail.com>
2021-10-09 23:24:08 +0330

    about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-           <h2>About page</h2> 
-        </div>
-    )
-}
+  return (
+    <section className="section about-section">
+      <h2 className='section-title'>about page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, earum.
+        Sequi facere doloremque assumenda, illo sapiente ad consequuntur modi
+        ducimus, architecto quae aspernatur id tempore sunt rem reiciendis!
+        Cupiditate quisquam obcaecati, officia provident neque ipsa enim
+        perferendis error recusandae sit.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
